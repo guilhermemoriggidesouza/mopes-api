fe16df532470a6218813d84087a87311781ef779
Guilherme Moriggi de Souza <guilhermemoriggidesouza@gmail.com>
2022-05-28 20:59:32 -0300

    fix: deploy application

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
     configDotenv()
     const app = await NestFactory.create(AppModule);
     app.enableCors();
-    await app.listen(process.env.PORT);
+    await app.listen(process.env.PORT || 80);
 }
 bootstrap();
