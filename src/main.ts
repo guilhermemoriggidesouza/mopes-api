fe9b1e9c3806b03c36fc2cc14dec418b1e75e999
Guilherme Moriggi de Souza <guilhermemoriggidesouza@gmail.com>
2022-05-28 21:01:29 -0300

    fix: modify port

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
     configDotenv()
     const app = await NestFactory.create(AppModule);
     app.enableCors();
-    await app.listen(process.env.PORT || 80);
+    await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
