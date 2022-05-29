b6ef3de8e28b29ed2a0812ad1a533ad4aee9747c
Guilherme Moriggi de Souza <guilhermemoriggidesouza@gmail.com>
2022-05-29 11:34:18 -0300

    fix connection with database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,19 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './User/User.entity';
 import { UserModule } from './User/User.module';
+import { config as configDotenv } from 'dotenv'
+configDotenv()
 
 @Module({
     imports: [
         UserModule,
         TypeOrmModule.forRoot({
             type: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            username: 'root',
-            password: 'root',
-            database: 'mope',
+            host: process.env.HOST_DB_MOPE,
+            port: parseInt(process.env.PORT_DB_MOPE),
+            username: process.env.USER_DB_MOPE,
+            password: process.env.PASSWORD_DB_MOPE,
+            database: process.env.DATABASE_DB_MOPE,
             entities: [User],
             synchronize: true,
         }),
