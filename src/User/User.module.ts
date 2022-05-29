27e6bf710d70675b0ed05604024a1a2735da6ba4
Guilherme Moriggi de Souza <guilhermemoriggidesouza@gmail.com>
2022-05-29 11:21:03 -0300

    feat: adding new total functional module

--- src/User/User.module.ts
+++ src/User/User.module.ts
@@ -1,9 +1,12 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { UserController } from './User.controller';
 import { UserService } from './User.service';
+import { User } from './User.entity';
 
 @Module({
     controllers: [UserController],
     providers: [UserService],
+    imports: [TypeOrmModule.forFeature([User])],
 })
 export class UserModule { }
