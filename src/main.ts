ba14b378d35a11d5acb5086869aefd2616293c14
Guilherme Moriggi de Souza <guilhermemoriggidesouza@gmail.com>
2022-07-04 20:25:18 -0300

    fix: removeCors

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config as configDotenv } from 'dotenv'
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
     configDotenv()
     const app = await NestFactory.create(AppModule);
-    app.enableCors();
     await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
