1cfb3f909b99e06a53eb21b61b2034cad3ad8c64
Guilherme Moriggi de Souza <guilhermemoriggidesouza@gmail.com>
2022-07-04 21:21:57 -0300

    fix: adding route login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,19 +4,22 @@ import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        private readonly userService: UserService,
-        private readonly jwtService: JwtService,
-    ) { }
-
-    async login(login: string, pass: string): Promise<any> {
-        const user = await this.userService.findOne({ where: { login } });
-        if (user && user.password === pass) {
-            const { password, ...result } = user;
-            return {
-                access_token: this.jwtService.sign(result),
-            };
-        }
-        throw new BadRequestException("Error on login")
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) { }
+  getRoute(role) {
+    return role == "admin" ? 'admin' : 'player'
+  }
+  async login(login: string, pass: string): Promise<any> {
+    const user = await this.userService.findOne({ where: { login } });
+    if (user && user.password === pass) {
+      const { password, ...result } = user;
+      return {
+        access_token: this.jwtService.sign(result),
+        route: this.getRoute(user.role)
+      };
     }
+    throw new BadRequestException("Error on login")
+  }
 }
