c90893e907378f570b7ebea2a0b18007ca087156
Guilherme Moriggi de Souza <guilhermemoriggidesouza@gmail.com>
2022-07-04 21:00:00 -0300

    fix: temove cors

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import { AppModule } from './app.module';
 import { config as configDotenv } from 'dotenv'
 
 async function bootstrap() {
-    configDotenv()
-    const app = await NestFactory.create(AppModule);
-    await app.listen(process.env.PORT || 3000);
+  configDotenv()
+  const app = await NestFactory.create(AppModule);
+  app.enableCors()
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
