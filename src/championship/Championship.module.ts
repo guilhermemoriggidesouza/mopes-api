7c5db5b2a6cc83e5a6aab5f0cc6534c0998cc805
Guilherme Moriggi <guilherme.moriggi@idwall.co>
2022-08-13 15:19:20 -0300

    fix sumula without service

--- src/championship/Championship.module.ts
+++ src/championship/Championship.module.ts
@@ -4,9 +4,13 @@ import { ChampionshipController } from './Championship.controller';
 import { ChampionshipService } from './Championship.service';
 import { Championship } from './entities/Championship.entity';
 import { ChampionshipKeys } from './entities/ChampionshipKeys.entity';
+import { SumulaModule } from 'src/sumula/Sumula.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Championship, ChampionshipKeys])],
+  imports: [
+    TypeOrmModule.forFeature([Championship, ChampionshipKeys]),
+    SumulaModule,
+  ],
   controllers: [ChampionshipController],
   providers: [ChampionshipService],
   exports: [ChampionshipService],
