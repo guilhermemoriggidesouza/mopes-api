21aabb02a3beb7bc0fae8e0689a398fda38e649a
Guilherme Moriggi <guilherme.moriggi@idwall.co>
2022-09-12 16:45:05 -0300

    fix: giant token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,19 +7,19 @@ export class AuthService {
   constructor(
     private readonly userService: UserService,
     private readonly jwtService: JwtService,
-  ) { }
+  ) {}
   getRoute(role) {
-    return role == "admin" ? 'admin' : 'player'
+    return role == 'admin' ? 'admin' : 'player';
   }
   async login(login: string, pass: string): Promise<any> {
     const user = await this.userService.findOne({ where: { login } });
     if (user && user.password === pass) {
-      const { password, ...result } = user;
+      const { password, teamsCreated, ...result } = user;
       return {
         access_token: this.jwtService.sign(result),
-        route: this.getRoute(user.role)
+        route: this.getRoute(user.role),
       };
     }
-    throw new BadRequestException("Error on login")
+    throw new BadRequestException('Error on login');
   }
 }
