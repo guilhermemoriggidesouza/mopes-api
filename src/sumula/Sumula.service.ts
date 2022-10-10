6b86b16c0ecccf119ef2763f02ebeef9678ae54c
Guilherme Moriggi <guilherme.moriggi@idwall.co>
2022-10-10 00:59:39 -0300

    feat: remove sumula infos

--- src/sumula/Sumula.service.ts
+++ src/sumula/Sumula.service.ts
@@ -182,6 +182,8 @@ export class SumulaService {
   }
 
   async remove({ id, where }: { id?: string; where?: any }): Promise<any> {
+    await this.playerInMatchRepository.delete({ sumulaId: parseInt(id) });
+    await this.statusGameRepository.delete({ sumulaId: parseInt(id) });
     return await this.sumulaRepository.delete(id || where);
   }
 
