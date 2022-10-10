2bf108430ac2a8421fd9d2c7835741a2d687fd57
Guilherme Moriggi <guilherme.moriggi@idwall.co>
2022-10-10 02:05:31 -0300

    feat: add teams to sumula request

--- src/championship/Championship.service.ts
+++ src/championship/Championship.service.ts
@@ -124,6 +124,7 @@ export class ChampionshipService {
         'category',
         'owner',
         'sumulas',
+        'sumulas.teams',
         'championshipKeys',
         'championshipKeys.sumulas',
         'championshipKeys.sumulas.teams',
