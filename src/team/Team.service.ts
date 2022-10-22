601ab2cec92a2e0bf49531cbfab2c83455ec61ba
Guilherme Moriggi <guilherme.moriggi@idwall.co>
2022-10-22 14:38:14 -0300

    feat: add balance

--- src/team/Team.service.ts
+++ src/team/Team.service.ts
@@ -198,7 +198,7 @@ export class TeamService {
       LEFT JOIN championship_keys ON championship_keys.id = sumula."championshipKeysId"
       where championship.id = ${championshipId}
       GROUP by te.name, game.total, pointsRestrict.total, championship_keys.name, te.id
-      ORDER BY key ASC NULLS LAST, "balancePoints" DESC
+      ORDER BY key ASC NULLS LAST
     `);
     tableGame = tableGame.map((game) => ({
       ...game,
@@ -208,7 +208,9 @@ export class TeamService {
     tableGame = [...keys].flatMap((key) => {
       const gameKeys = tableGame.filter((game) => game.key == key);
       gameKeys.sort((teamA, teamB) =>
-        parseInt(teamA.point) < parseInt(teamB.point) ? 1 : -1,
+        parseInt(teamA.point) < parseInt(teamB.point)
+          ? 1
+          : -1 && parseInt(teamA.balancePoints) < parseInt(teamB.balancePoints),
       );
       return gameKeys;
     });
