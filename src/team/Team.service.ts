3719e57c39bc8df3c4798c3f58ec52ca43a36e78
Guilherme Moriggi <guilherme.moriggi@idwall.co>
2022-10-22 14:22:13 -0300

    feat: add new filters

--- src/team/Team.service.ts
+++ src/team/Team.service.ts
@@ -198,7 +198,7 @@ export class TeamService {
       LEFT JOIN championship_keys ON championship_keys.id = sumula."championshipKeysId"
       where championship.id = ${championshipId}
       GROUP by te.name, game.total, pointsRestrict.total, championship_keys.name, te.id
-      ORDER BY "balancePoints" DESC, key ASC NULLS LAST
+      ORDER BY key ASC NULLS LAST, "balancePoints" DESC
     `);
     tableGame = tableGame.map((game) => ({
       ...game,
