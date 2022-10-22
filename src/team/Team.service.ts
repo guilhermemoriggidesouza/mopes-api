e13ff36d554ad900e2a1fc206d9bfec987153164
Guilherme Moriggi <guilherme.moriggi@idwall.co>
2022-10-21 22:33:44 -0300

    feat: change order

--- src/team/Team.service.ts
+++ src/team/Team.service.ts
@@ -198,7 +198,7 @@ export class TeamService {
       LEFT JOIN championship_keys ON championship_keys.id = sumula."championshipKeysId"
       where championship.id = ${championshipId}
       GROUP by te.name, game.total, pointsRestrict.total, championship_keys.name, te.id
-      ORDER BY key ASC NULLS LAST, "pointsDontDoIt" DESC, "pointsDoIt" DESC, "balancePoints" DESC    
+      ORDER BY key ASC NULLS LAST, "balancePoints" DESC, "pointsDoIt" DESC, "pointsDontDoIt" DESC
     `);
     tableGame = tableGame.map((game) => ({
       ...game,
